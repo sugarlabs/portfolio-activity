165433ab2f4d3f04c0b5ba206f1b7e9349ca5147
Walter Bender <walter.bender@gmail.com>
2011-08-17 18:56:53 -0400

    jump to slide when leaving thumbsview

--- PortfolioActivity.py
+++ PortfolioActivity.py
@@ -444,7 +444,7 @@ class PortfolioActivity(activity.Activity):
 
     def _thumbs_cb(self, button=None):
         if self._thumbnail_mode:
-            self._set_view_mode()
+            self._set_view_mode(self.i)
             self._show_slide(self._current_slide)
         else:
             self._stop_autoplay()

4a5696706e843ec7083b1928e58d39a9ff1eccf2
James Cameron <quozl@laptop.org>
2017-06-21 17:55:50 +1000

    Fix Python GI API version warnings

--- PortfolioActivity.py
+++ PortfolioActivity.py
@@ -11,6 +11,9 @@
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('PangoCairo', '1.0')
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf

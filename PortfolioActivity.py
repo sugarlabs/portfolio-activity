d87289fc7c5e41951fe627e6c5cb5a46d027a230
James Cameron <quozl@laptop.org>
2018-08-02 13:12:41 +1000

    Fix license addresses

--- PortfolioActivity.py
+++ PortfolioActivity.py
@@ -8,8 +8,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this library; if not, write to the Free Software
-# Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
-
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
 
 import gi
 gi.require_version('Gtk', '3.0')
